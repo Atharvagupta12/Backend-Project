6aa248e6902de0e8e9591ab58ed2111d6a81ed36
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-05-08 00:05:08 +0530

    added Stage and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,16 +59,29 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  jwt.sign({
-    _id: this._id,
-    email: this.emial,
-    username: this.username,
-    fullName: this.fullName,
-  }
-
-)
-
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.emial,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
 };
-userSchema.methods.generateRefreshToken = function () {};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
