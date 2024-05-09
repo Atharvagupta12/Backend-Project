efe7ba159445363bd46adec05d8eb2723819d045
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-05-10 03:30:38 +0530

    all config

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+  })
+);
 
-app.use(express.json({extended:true, limit: "16kb"}))
-app.use(express.urlencoded({extended: true,limit:"16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ extended: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app}
+// routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+export { app };
